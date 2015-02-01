f5affb5bfcc31f9ef5c63c28e2cc9a93dd9d4549
hudzanton <gudz.anton@gmail.com>
2015-02-01 21:12:04 +0200

    Finished MyHashMapApp.

--- MyHashMapApp/app/src/main/java/com/example/hudzanton/myhashmapapp/MainActivity.java
+++ MyHashMapApp/app/src/main/java/com/example/hudzanton/myhashmapapp/MainActivity.java
@@ -6,24 +6,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class MainActivity extends ActionBarActivity {
     private Map<String, String> hashMap = new HashMap<String, String>();
     private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.editText = (EditText) this.findViewById(R.id.editText);
         this.setContentView(R.layout.activity_main);
-        this.hashMap.put("Key One", "Key One Value");
-        this.hashMap.put("Key Two", "Key Two Value");
-        this.hashMap.put("Key Three", "Key Three Value");
+        this.editText = (EditText) this.findViewById(R.id.editText);
+        this.hashMap.put("one", "Key One Value");
+        this.hashMap.put("two", "Key Two Value");
+        this.hashMap.put("three", "Key Three Value");
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,8 +43,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickCheckKeyButton(View view) {
-        if (this.hashMap.containsKey(this.editText.getText().toString())){
-            this.editText.setText(String.valueOf(this.hashMap.values()));
+        String key = this.editText.getText().toString();
+        if (this.hashMap.containsKey(key)){
+            String value = this.hashMap.get(key);
+            this.editText.setText(String.valueOf(value));
         }
         else this.editText.setText ("Entered key is absent");
     }
